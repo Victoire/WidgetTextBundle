7eba3ee33137eb891c7b4061993b0e6f673351b0
Paul Andrieux <paul@appventus.com>
2013-11-27 23:37:08 +0100

    add translations

---
+++ Translation/DynamicTranslations.php
@@ -0,0 +1,20 @@
+<?php
+namespace Victoire\TextBundle\Translation;
+
+use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+use JMS\TranslationBundle\Model\FileSource;
+
+class DynamicTranslations implements TranslationContainerInterface
+{
+    /**
+     * usage example: new Message('example.keymap')->addSource('path/to/source/file', '514', '10'),
+     * @return array the keys to register in jms translation
+     */
+    public static function getTranslationMessages()
+    {
+        return array(
+            new Message('widget.text.new.action.label'),
+        );
+    }
+}
